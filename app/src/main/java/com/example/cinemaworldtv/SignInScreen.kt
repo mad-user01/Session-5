baa1ea9713a95324de9cf512e4ca633e954bdfb9
ZhdanovichVladimir <adtspb.mad.user01@gmail.com>
2022-02-10 12:12:40 +0300

    second

--- app/src/main/java/com/example/cinemaworldtv/SignInScreen.kt
+++ app/src/main/java/com/example/cinemaworldtv/SignInScreen.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import kotlinx.android.synthetic.main.activity_sign_in_screen.*
 
 class SignInScreen : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,17 @@ class SignInScreen : FragmentActivity() {
     }
 
     fun login(view: View) {
+        val x = email12.getText().toString()
+        val y = textView.getText().toString()
+        if (x.isEmpty() or y.isEmpty()){
+            val text = "Пустые поля!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+        else{
         val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+        startActivity(intent)}
     }
 }
